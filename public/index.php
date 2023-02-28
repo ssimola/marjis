bcd7f53c3eae38d1225c050fc42b50a11cf9c4f0
Sami Simola <sami.simola@edu.sasky.fi>
2023-02-28 10:01:12 +0200

    hakemistoviittaukset vakioiksi

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@
 
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
   // Luodaan uusi Plates-olio ja kytketään se sovelluksen sivupohjiin.
-  $templates = new League\Plates\Engine('../src/view'); 
+  $templates = new League\Plates\Engine(TEMPLATE_DIR); 
 
   // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava
   // käsittelijä.
