8f830f4abe3bfea0b52a23a2edf8e6c5d3ff4068
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-06 14:01:26 +0200

    yksittäisen tapahtuman tietojen tulostaminen

--- src/model/tapahtuma.php
+++ src/model/tapahtuma.php
@@ -1,9 +1,11 @@
 <?php
-
   require_once HELPERS_DIR . 'DB.php';
-
   function haeTapahtumat() {
     return DB::run('SELECT * FROM marjistapahtuma ORDER BY tap_alkaa;')->fetchAll();
+  }
+
+  function haeTapahtuma($id) {
+    return DB::run('SELECT * FROM marjistapahtuma WHERE idtapahtuma = ?;',[$id])->fetch();
   }
 
 ?>
