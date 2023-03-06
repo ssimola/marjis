16933732c47563c4e95710b48739f38eb9494f02
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-06 14:07:32 +0200

    linkitykset tapahtuma-sivulle ja etusivulle

--- config/config.php
+++ config/config.php
@@ -16,6 +16,8 @@ $config = array(
   define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
   define("MODEL_DIR", PROJECT_ROOT . "src/model/");
   define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
+  define("BASEURL", $config['urls']['baseUrl']);
+
 
 
 ?>
--- src/view/template.php
+++ src/view/template.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">    
   </head>
   <body>
-    <header>
-      <h1>Marjaniemi</h1>
+  <header>
+      <h1><a href="<?=BASEURL?>">marjis</a></h1>
     </header>
     <section>
       <?=$this->section('content')?>
