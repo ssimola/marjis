fac3c5b11079b3551dc76eb10a74f5b0080f1067
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-07 18:27:20 +0200

    lomaketiedon siistiminen

--- public/index.php
+++ public/index.php
@@ -30,9 +30,10 @@
       break;
     case '/lisaa_tili':
       if (isset($_POST['laheta'])) {
+        $formdata = cleanArrayData($_POST);
         require_once MODEL_DIR . 'henkilo.php';
-        $salasana = password_hash($_POST['salasana1'], PASSWORD_DEFAULT);
-        $id = lisaaHenkilo($_POST['nimi'],$_POST['email'],$_POST['mokki'],$salasana);
+        $salasana = password_hash($formdata['salasana1'], PASSWORD_DEFAULT);
+        $id = lisaaHenkilo($formdata['nimi'],$formdata['email'],$formdata['mokki'],$salasana);
         echo "Tili on luotu tunnisteella $id";
         break;
       } else {
