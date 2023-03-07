7ea0fae3e919199d703c125b576ab93e47d61ac5
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-07 19:22:54 +0200

    tili luotu -sivupohja

--- public/index.php
+++ public/index.php
@@ -34,7 +34,7 @@
         require_once CONTROLLER_DIR . 'tili.php';
         $tulos = lisaaTili($formdata);
         if ($tulos['status'] == "200") {
-          echo "Tili on luotu tunnisteella $tulos[id]";
+          echo $templates->render('tili_luotu', ['formdata' => $formdata]);
           break;
         }
         echo $templates->render('lisaa_tili', ['formdata' => $formdata, 'error' => $tulos['error']]);
