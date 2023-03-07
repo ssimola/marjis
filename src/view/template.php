0d4dff50756c5c396a4cbf60dd6e1fab71de0c7a
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-07 09:22:31 +0200

    sivuston tyylimääritykset

--- src/view/template.php
+++ src/view/template.php
@@ -2,11 +2,13 @@
 <html lang="fi">
   <head>
     <title>marjis - <?=$this->e($title)?></title>
-    <meta charset="UTF-8">    
+    <meta charset="UTF-8">
+    <link href="styles/styles.css" rel="stylesheet"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
   </head>
   <body>
   <header>
-      <h1><a href="<?=BASEURL?>">marjis</a></h1>
+      <h1><a href="<?=BASEURL?>"><img src="images/logo.jpg"/></a></h1>
     </header>
     <section>
       <?=$this->section('content')?>
