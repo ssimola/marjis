25bea7e475f9aadae72e5b939da9e34f9c0e66fe
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-09 10:00:58 +0200

    kirjautumistietojen tulostus

--- src/view/template.php
+++ src/view/template.php
@@ -9,6 +9,16 @@
   <body>
   <header>
       <h1><a href="<?=BASEURL?>"><img src="images/logo.jpg"/></a></h1>
+      <div class="profile">
+        <?php
+          if (isset($_SESSION['user'])) {
+            echo "<div>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          }
+        ?>
+      </div>
     </header>
     <section>
       <?=$this->section('content')?>
