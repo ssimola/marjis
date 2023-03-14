9a5ca68c037ffedef9f70892cd451b5ac185c9b4
Sami Simola <sami.simola@edu.sasky.fi>
2023-03-14 08:48:19 +0200

    vaihtoavaimen lisääminen henkilo-tauluun

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -19,4 +19,17 @@
     return DB::run('UPDATE marjishenkilo SET vahvistettu = TRUE WHERE vahvavain = ?', [$avain])->rowCount();
   }
 
+  function asetaVaihtoavain($email,$avain) {
+    return DB::run('UPDATE marjishenkilo SET nollausavain = ?, nollausaika = NOW() + INTERVAL 30 MINUTE WHERE email = ?', [$avain,$email])->rowCount();
+  }
+
+  function tarkistaVaihtoavain($avain) {
+    return DB::run('SELECT nollausavain, nollausaika-NOW() AS aikaikkuna FROM marjishenkilo WHERE nollausavain = ?', [$avain])->fetch();
+  }
+
+  function vaihdaSalasanaAvaimella($salasana,$avain) {
+    return DB::run('UPDATE marjishenkilo SET salasana = ?, nollausavain = NULL, nollausaika = NULL WHERE nollausavain = ?', [$salasana,$avain])->rowCount();
+  }
+
+
 ?>
